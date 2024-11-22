33b631cd222966518cbfacd5fa04a37d483fc1a1
Elizabeth <lizagotovitryby@gmail.com>
2024-11-21 22:02:12 -0500

    added ResultsPage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.scss'
 import HomePage from './pages/HomePage/HomePage'
+import ResultsPage from "./pages/ResultsPage/ResultsPage";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
           <Route path="/" element={<HomePage/>} />
           <Route
             path="/result"
-            element={<></>}
+            element={<ResultsPage />}
           />
         </Routes>
       </main>
