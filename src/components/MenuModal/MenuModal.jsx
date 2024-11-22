6feec0926f2730f5620f4e9c65402c6cfbc167a3
Shina Ng <ShinaLNYK@gmail.com>
2024-11-21 22:53:53 -0500

    Make FeatureBlock component

--- src/components/MenuModal/MenuModal.jsx
+++ src/components/MenuModal/MenuModal.jsx
@@ -14,33 +14,45 @@ function MenuModal({ isOpen, handleCloseModal }) {
                     alignItems: "flex-end",
                     background: "rgba(19, 24, 44, 0.7)",
                 },
+                content: {
+                    backgroundColor: "none",
+                    border: "none",
+                },
             }}
             // Attach modal to the main.jsx element (with id root)
             appElement={document.getElementById("root")}
         >
             <div className="modal__content-wrapper">
-                <hr className="modal__swipe-bar" />
-                <div>
-                    <div>
-                        <img src="" alt="Bookmark icon" />
-                    </div>
-                    <div>
-                        <img src="" alt="Remix icon" />
-                    </div>
+                <hr className="modal__bar modal__bar--top" />
+                <div className="modal__features">
+                    <article className="feature__save-wrapper">
+                        <span class="material-icons">bookmark_border</span>
+                        <span className="feature__text feature__text--top">
+                            Save
+                        </span>
+                    </article>
+
+                    <article className="feature__remix-wrapper">
+                        <span className="material-icons"></span>
+                        <span className="feature__text feature__text--top">
+                            Remix
+                        </span>
+                    </article>
                 </div>
                 <button className="modal__visual-search-btn">
                     Visual Search
                 </button>
-                <section>
-                    <div className="modal__function"></div>
-                    <div className="modal__function"></div>
-                    <div className="modal__function"></div>
-                    <div className="modal__function"></div>
-                    <div className="modal__function"></div>
-                    <div className="modal__function"></div>
-                    <div className="modal__function"></div>
+                <section className="modal__function-list">
+                    <div className="modal__function-item"></div>
+                    <div className="modal__function-item"></div>
+                    <div className="modal__function-item"></div>
+                    <div className="modal__function-item"></div>
+                    <div className="modal__function-item"></div>
+                    <div className="modal__function-item"></div>
+                    <div className="modal__function-item"></div>
                 </section>
-                <div className="modal__settings"></div>
+                <article className="modal__settings"></article>
+                <hr className="modal__bar modal__bar--bottom" />
             </div>
         </ReactModal>
     );
